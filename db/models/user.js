edf69e325934b2ee7166e31454ac602e7ac6e529
gagata <agataxgolebiowska@gmail.com>
2024-06-20 23:37:36 +0200

    Login

---
+++ db/models/user.js
@@ -0,0 +1,23 @@
+const db = require('../db');
+const bcrypt = require('bcrypt');
+
+async function login(username, password) {
+    return new Promise((resolve, reject) => {
+        db.get('SELECT * FROM users WHERE email = ?', [username], async (err, user) => {
+            if (err) {
+                reject(err); 
+            } else if (!user) {
+                reject(new Error('Invalid username or password'));
+            } else {
+                const isMatch = await bcrypt.compare(password, user.password);
+                if (isMatch) {
+                    resolve(user);
+                } else {
+                    reject(new Error('Invalid username or password'));
+                }
+            }
+        });
+    });
+}
+
+module.exports = { createUser, login };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const { randomUUID } = require('crypto');
+const { createUser, login } = require('../db/models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+/**
+* Endpoint POST /login - Loguje użytkownika.
+*
+*
+* @param {Object} req - Obiekt żądania HTTP.
+* @param {Object} res - Obiekt odpowiedzi HTTP.
+*/
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const sessionId = req.body.sessionId || randomUUID();
+
+  try {
+      const user = await login(email, password);
+      res.json({ message: 'Login successful', user, sessionId });
+  } catch (err) {
+      console.error(err);
+      res.status(401).json({ error: err.message });
+  }
 });
 
 module.exports = router;
